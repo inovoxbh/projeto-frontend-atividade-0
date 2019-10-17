0c94549834905d299562802e7a4dbcd2a0be044b
Bruno G. Alcantara <inovox@gmail.com>
2019-10-17 00:16:55 -0300

    primeira versão funcionando

--- js/javascript.js
+++ js/javascript.js
@@ -1,39 +1,48 @@
 $(document).ready(function(){
-    var db;
-    var request;
-    var store;
-    
-    request= indexedDB.open("BrowserSQL", 1);
-
-    request.onerror = function (e) { 
-        console.log('Não foi possível abrir o banco de dados. Erro: ' + e.target.error);
-    }
-
-    request.onupgradeneeded = function(e) {
-        db = e.target.result;
-        store =db.createObjectStore("Visitantes", { keyPath: "CPF" });
-    }
-
-    request.onsuccess =function(e) {
-        db = e.target.result;
-        console.log('deu certo');
-    }
-
-    $("#gravar").click(function(){
-        var cpf = $("#InputCPF").val();
-        var nome = $("#InputNome").val();
-    
-        var transaction = db.transaction(["Visitantes"],"readwrite");
-    
-        transaction.onerror = function(e) {
-              console.log('Não foi possível abrir transação com o banco de dados. Erro: ' + e.target.error);
-        };  
-    
-        transaction.oncomplete = function(e) {
-            console.log("Sucesso :)");
-        };
-        
-        var store = transaction.objectStore("Visitantes");
-        store.add({CPF: cpf, Nome : nome});
-    })
+	window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+	var request, db;
+	if(!window.indexedDB)
+	{
+		console.log("Seu navegador não suporta o recurso HTML5 IndexedDB");
+	}
+	else
+	{
+		request = window.indexedDB.open("SQLBrowser2", 1);
+		request.onerror = function(event){
+			console.log("Erro ao abrir o banco de dados", event);
+		}
+	
+		request.onupgradeneeded = function(event){
+			console.log("Atualizando");
+			db = event.target.result;
+            var objectStore = db.createObjectStore("visitantes", { keyPath : 'cpf'});
+		};
+		request.onsuccess = function(event){
+			console.log("Banco de dados aberto com sucesso");
+			db = event.target.result;
+		}
+	}
+	$("#gravar").click(function(){
+		var nome = $("#InputNome").val();
+		var cpf = $("#InputCPF").val();
+		var transaction = db.transaction(["visitantes"],"readwrite");
+		transaction.oncomplete = function(event) {
+			console.log("Sucesso :)");
+			$("#feedback").html("Adicionado com Sucesso");
+		};
+		transaction.onerror = function(event) {
+			console.log("Erro :(");
+			$("#feedback").html("Erro ao Adicionar");
+		};
+		var objectStore = transaction.objectStore("visitantes");
+		objectStore.add({cpf: cpf, nome: nome});
+	});
+	
+	$("#consultar").click(function(){
+		var codigo = $("#InputCPF").val();
+		var request = db.transaction(["visitantes"],"readwrite").objectStore("visitantes").get(codigo);
+		request.onsuccess = function(event){
+			$("#feedback").html("Nome : "+request.result.nome);
+		};
+	});
 });
